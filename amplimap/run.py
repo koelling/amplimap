bf188616ec1fad566409befa61bcfc969a9f4415
Nils Koelling <nk@nk.gl>
2018-05-15 13:48:09 +0100

    0.2.7 - fix argv issue

--- amplimap/run.py
+++ amplimap/run.py
@@ -75,7 +75,14 @@ def main(argv = None):
         parser.add_argument("--debug", help="debug mode", action="store_true")
         #parser.add_argument("--debug-dag", help="debug DAG", action="store_true")
         parser.add_argument("TARGET", help="targets to run (eg. pileups variants coverages)", nargs="*")
-        args = parser.parse_args(argv)
+        if argv is None:
+            args = parser.parse_args()
+        else:
+            args = parser.parse_args(argv)
+
+        if args.debug:
+            print('Incoming argv: {}'.format(str(argv)))
+            print('Targets: {}'.format(str(args.TARGET)))
 
         if args.version:
             print('{} {}'.format(__title__, __version__))
