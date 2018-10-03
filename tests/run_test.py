7ab15e248fdfa0cf0e10e88beb1f859034fbf482
Nils Koelling <nk@nk.gl>
2018-10-03 15:58:15 +0100

    fix capsys

--- tests/run_test.py
+++ tests/run_test.py
@@ -70,7 +70,7 @@ def test_config(capsys):
     #     sys.stdout.write(captured.err)
     #     sys.stdout.write(captured.out)
 
-def check_run(wd_path):
+def check_run(capsys, wd_path):
     #dry-run
     amplimap.run.main(['--working-directory={}'.format(wd_path), 'pileups'])
     captured = capsys.readouterr()
@@ -122,7 +122,7 @@ def test_naive_pileups(capsys):
     wd_path = os.path.join(packagedir, "sample_data", "wd_naive")
     init_wd(wd_path, os.path.join(packagedir, "sample_data", "sample_reads_in"))
 
-    check_run(wd_path)
+    check_run(capsys, wd_path)
     check_default_stats(wd_path)
     check_default_pileups(wd_path)
 
@@ -130,7 +130,7 @@ def test_bwa_pileups(capsys):
     wd_path = os.path.join(packagedir, "sample_data", "wd_bwa")
     init_wd(wd_path, os.path.join(packagedir, "sample_data", "sample_reads_in"))
 
-    check_run(wd_path)
+    check_run(capsys, wd_path)
     check_default_stats(wd_path)
     check_default_pileups(wd_path)
 
@@ -138,7 +138,7 @@ def test_raw_read_pileups(capsys):
     wd_path = os.path.join(packagedir, "sample_data", "wd_bwa_raw")
     init_wd(wd_path, os.path.join(packagedir, "sample_data", "sample_reads_in"))
 
-    check_run(wd_path)
+    check_run(capsys, wd_path)
 
     #check custom stats
     samples = pd.read_csv(os.path.join(wd_path, 'analysis', 'reads_parsed', 'stats_samples.csv'))
@@ -156,7 +156,7 @@ def test_umi_pileups(capsys):
     wd_path = os.path.join(packagedir, "sample_data", "wd_umis")
     init_wd(wd_path, os.path.join(packagedir, "sample_data", "sample_reads_in"), umi_one = 3, umi_two = 4)
 
-    check_run(wd_path)
+    check_run(capsys, wd_path)
     check_default_stats(wd_path)
     check_default_pileups(wd_path, expected_coverage=4) #one less, because two read pairs have same umi!
 
